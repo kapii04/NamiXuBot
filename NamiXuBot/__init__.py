38a63fa0a02307dd3ca20bfeb60683218dbb2b6d
kapii04 <119713755+kapii04@users.noreply.github.com>
2022-12-23 12:20:20 +0700

    Update __init__.py

--- NamiXuBot/__init__.py
+++ NamiXuBot/__init__.py
@@ -34,7 +34,7 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="Zaid/modules/bot"),
+    plugins=dict(root="NamiXuBot/modules/bot"),
     in_memory=True,
 )
 
